78e7ddb7de9e2e1418592df88a414d30ad5b6f70
neva.wood <nevalu@gmail.com>
2016-06-17 10:13:32 -0700

    jonathan was upset so i made it shorter

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,24 @@
 $(document).ready(function() {
     $("form#counter").submit(function(event) {
         event.preventDefault();
-
-
         var inNum = [$("input#number").val()];
-
         var outNum = [];
         for (var i = 1; i <= inNum; i++) {
             outNum.push(i);
-
-
         }
 
-        console.log(outNum); // [0, 1, 2]
+        function pingpong(a, b) {
             for (var i = 1; i < outNum.length; i++)
-                if (outNum[i] % 15 === 0)
-                    outNum[i] = "PingPong";
-            for (var i = 1; i < outNum.length; i++)
-                if (outNum[i] % 3 === 0)
-                    outNum[i] = "ping";
-            for (var i = 1; i < outNum.length; i++)
-                if (outNum[i] % 5 === 0)
-                    outNum[i] = "pong";
-
-
+                if (outNum[i] % a === 0)
+                    outNum[i] = b;
+        }
+        pingpong(15, "pingpong");
+        pingpong(5, "pong");
+        pingpong(3, "ping");
 
         var html = "";
-            for (var i = 0; i < outNum.length; i++)
-                html += "<li>" + outNum[i]+ "</li>";
-
+        for (var i = 0; i < outNum.length; i++)
+            html += "<li>" + outNum[i] + "</li>";
         console.log(outNum);
         document.getElementById("list").innerHTML = html;
         // $("ul#list").prepend("<li><span class='game'>" + [outNum] + "</span></li>");
